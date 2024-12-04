b742e7990ae280e10f90d8b249006ff30e6a2765
zaaron345 <zaaron345@gmail.com>
2024-12-04 19:09:17 +0100

    Updates

---
+++ precache-manifest.c7235fc23abf51a7f39b7bd7b03a3c82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83dbc71ccdcfe56a0154fa1ca595275f",
+    "url": "/transcript-audio-sync/index.html"
+  },
+  {
+    "revision": "587647d818b380c259b3",
+    "url": "/transcript-audio-sync/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "19b653926d9b087a2e07",
+    "url": "/transcript-audio-sync/static/js/2.5ff95bdd.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/transcript-audio-sync/static/js/2.5ff95bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/transcript-audio-sync/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "587647d818b380c259b3",
+    "url": "/transcript-audio-sync/static/js/main.bb65c50c.chunk.js"
+  },
+  {
+    "revision": "71ee7a2f45045440c6d5",
+    "url": "/transcript-audio-sync/static/js/runtime-main.b4e1cde7.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/transcript-audio-sync/static/media/logo.06e73328.svg"
+  }
+]);
