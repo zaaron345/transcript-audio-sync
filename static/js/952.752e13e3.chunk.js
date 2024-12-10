3dd9be46fe93610c264485b41dddd0f4d359041f
zaaron345 <zaaron345@gmail.com>
2024-12-10 22:18:51 +0100

    Updates

---
+++ static/js/952.752e13e3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.752e13e3.chunk.js.map
